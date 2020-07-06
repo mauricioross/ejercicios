0416e84e1c27312759b9c5f5450b44df7e194afd
mauricio_ross <fxmross@gmail.com>
2020-07-06 03:55:01 -0400

    Updates

---
+++ precache-manifest.0666ea7bfc9118ae160ac1c2c8750c8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "336e87150b81836e868539a0ce94f22a",
+    "url": "/ejercicios/index.html"
+  },
+  {
+    "revision": "25559ccecfdc5d516275",
+    "url": "/ejercicios/static/css/main.53c46bac.chunk.css"
+  },
+  {
+    "revision": "99f7096008157eb972b4",
+    "url": "/ejercicios/static/js/2.a66b0eea.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ejercicios/static/js/2.a66b0eea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25559ccecfdc5d516275",
+    "url": "/ejercicios/static/js/main.2dbc3dfd.chunk.js"
+  },
+  {
+    "revision": "0c5850ed1f010f863707",
+    "url": "/ejercicios/static/js/runtime-main.6a2c0b4d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ejercicios/static/media/logo.5d5d9eef.svg"
+  }
+]);
